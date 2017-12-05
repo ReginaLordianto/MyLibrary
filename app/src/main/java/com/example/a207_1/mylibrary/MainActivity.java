4564a5b490439b8f21c140b87636236a198430fc
evelynwijaya <32315692+evelynwijaya@users.noreply.github.com>
2017-12-04 20:18:34 -0800

    Initial commit

---
+++ app/src/main/java/com/example/a207_1/mylibrary/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a207_1.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText dt_edt1;
+    private 
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
